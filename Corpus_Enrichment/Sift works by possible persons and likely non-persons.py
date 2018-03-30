9e43c176b112e422407378dc8261ccd6a6c004f0
eslao <christine.eslao@gmail.com>
2018-03-30 09:15:58 -0400

    Fix logic

--- Corpus_Enrichment/Sift works by possible persons and likely non-persons.py
+++ Corpus_Enrichment/Sift works by possible persons and likely non-persons.py
@@ -6,9 +6,20 @@ likely_persons = []
 
 with open(file) as name_csv:
     reader = csv.DictReader(name_csv)
-    for row in reader:
-        if re.search('[1-9]{4}-[1-9]{4}', row["authorGroup"]) is None: # look for birth/death date ranges, as in "Pufendorf, Samuel, Freiherr von           1632           1694           1632-1694"           
-            possible_non_persons += [row]
-        elif re.search('[\w\s]+\.\s[\w\s]*', row["authorGroup"]) is None: # look for hierarchical names, like "England and Wales. Parliament. House of Commons"     
-            possible_non_persons += [row]
-        else: likely_persons += [row]
+    for row in reader:        
+        if re.search('[1-9]{4}-[1-9]{4}', row["authorGroup"]) is not None: # look for birth/death date ranges, as in "Pufendorf, Samuel, Freiherr von           1632           1694           1632-1694"         
+            likely_persons += [row]
+            print("Is this a person?")
+            print(row["authorGroup"])
+        elif re.search('[\w\s]+\.\s[\w\s]*', row["authorGroup"]) is None: # look for hierarchical names, like "England and Wales. Parliament. House of Commons"   
+            likely_persons += [row]
+            print("Is this a person?")
+            print(row["authorGroup"])
+        elif re.search('[\w\s]+\.\s[\w\s]*', row["authorGroup"]) is None:
+            likely_persons += [row]
+            print("Is this a person?")
+            print(row["authorGroup"])
+        else:
+            possible_non_persons += [row] #to do: write a function for filtering specific stopwords, such as "company"
+            print("Is this a non-person?")
+            print(row["authorGroup"])
