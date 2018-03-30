3b4af3c4bde7b15eecb84233f1e25aff9e107d2a
eslao <christine.eslao@gmail.com>
2018-03-30 09:19:20 -0400

    Comment out debugging lines

--- Corpus_Enrichment/Sift works by possible persons and likely non-persons.py
+++ Corpus_Enrichment/Sift works by possible persons and likely non-persons.py
@@ -9,17 +9,17 @@ with open(file) as name_csv:
     for row in reader:        
         if re.search('[1-9]{4}-[1-9]{4}', row["authorGroup"]) is not None: # look for birth/death date ranges, as in "Pufendorf, Samuel, Freiherr von           1632           1694           1632-1694"         
             likely_persons += [row]
-            print("Is this a person?")
-            print(row["authorGroup"])
+            #print("Is this a person?")
+            #print(row["authorGroup"])
         elif re.search('[\w\s]+\.\s[\w\s]*', row["authorGroup"]) is None: # look for hierarchical names, like "England and Wales. Parliament. House of Commons"   
             likely_persons += [row]
-            print("Is this a person?")
-            print(row["authorGroup"])
+            #print("Is this a person?")
+            #print(row["authorGroup"])
         elif re.search('[\w\s]+\.\s[\w\s]*', row["authorGroup"]) is None:
             likely_persons += [row]
-            print("Is this a person?")
-            print(row["authorGroup"])
+            #print("Is this a person?")
+            #print(row["authorGroup"])
         else:
             possible_non_persons += [row] #to do: write a function for filtering specific stopwords, such as "company"
-            print("Is this a non-person?")
-            print(row["authorGroup"])
+            #print("Is this a non-person?")
+            #print(row["authorGroup"])
