972cc2976efc6a5848f3045e7b3cd5479efba4b7
eslao <christine.eslao@gmail.com>
2018-03-30 08:39:54 -0400

    Create Sift works by possible persons and likely non-persons.py
    Not at all elegant

---
+++ Corpus_Enrichment/Sift works by possible persons and likely non-persons.py
@@ -0,0 +1,14 @@
+import csv, re
+file = "ecco_data_combined_metadata.csv"
+
+possible_non_persons = []
+likely_persons = []
+
+with open(file) as name_csv:
+    reader = csv.DictReader(name_csv)
+    for row in reader:
+        if re.search('[1-9]{4}-[1-9]{4}', row["authorGroup"]) is None: # look for birth/death date ranges, as in "Pufendorf, Samuel, Freiherr von           1632           1694           1632-1694"           
+            possible_non_persons += row
+        elif re.search('[\w\s]+\.\s[\w\s]*', row["authorGroup"]) is None: # look for hierarchical names, like "England and Wales. Parliament. House of Commons"     
+            possible_non_persons += row
+        else: likely_persons += row
