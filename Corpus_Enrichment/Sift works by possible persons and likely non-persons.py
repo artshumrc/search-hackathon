73294063241569a0232c36133168f0387bea1ff3
eslao <christine.eslao@gmail.com>
2018-03-30 08:42:56 -0400

    Add missing brackets

--- Corpus_Enrichment/Sift works by possible persons and likely non-persons.py
+++ Corpus_Enrichment/Sift works by possible persons and likely non-persons.py
@@ -8,7 +8,7 @@ with open(file) as name_csv:
     reader = csv.DictReader(name_csv)
     for row in reader:
         if re.search('[1-9]{4}-[1-9]{4}', row["authorGroup"]) is None: # look for birth/death date ranges, as in "Pufendorf, Samuel, Freiherr von           1632           1694           1632-1694"           
-            possible_non_persons += row
+            possible_non_persons += [row]
         elif re.search('[\w\s]+\.\s[\w\s]*', row["authorGroup"]) is None: # look for hierarchical names, like "England and Wales. Parliament. House of Commons"     
-            possible_non_persons += row
-        else: likely_persons += row
+            possible_non_persons += [row]
+        else: likely_persons += [row]
