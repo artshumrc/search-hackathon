ca73df91c3418590502ca091bfc98fc26c46325d
eslao <christine.eslao@gmail.com>
2018-03-30 09:46:37 -0400

    Update Sift works by possible persons and likely non-persons.py

--- Corpus_Enrichment/Sift works by possible persons and likely non-persons.py
+++ Corpus_Enrichment/Sift works by possible persons and likely non-persons.py
@@ -3,26 +3,23 @@ file = "ecco_data_combined_metadata.csv"
 
 possible_non_persons = []
 likely_persons = []
-
-with open(file) as name_csv:
-    reader = csv.DictReader(name_csv)
-    for row in reader:        
-        if re.search('[1-9]{4}-[1-9]{4}', row["authorGroup"]) is not None: # look for birth/death date ranges, as in "Pufendorf, Samuel, Freiherr von           1632           1694           1632-1694"         
+ 
+        if re.search('[1-9]{4}-[1-9]{4}', row["authorGroup"]) is not None: # look for birth/death date ranges, as in "Pufendorf, Samuel, Freiherr von           1632           1694           1632-1694" 
             likely_persons += [row]
-            #print("Is this a person?")
-            #print(row["authorGroup"])
-        elif re.search('[\w\s]+\.\s[\w\s]*', row["authorGroup"]) is None: # look for hierarchical names, like "England and Wales. Parliament. House of Commons"   
+            print("Is this a person?")
+            print(row["authorGroup"])
+        elif re.search('[dl]\.\s[1-9]{4}', row["authorGroup"]) is not None: # look for death/flourishing dates
             likely_persons += [row]
-            #print("Is this a person?")
-            #print(row["authorGroup"])
-        elif re.search('[\w\s]+\.\s[\w\s]*', row["authorGroup"]) is None:
+            print("Is this a person?")
+            print(row["authorGroup"])        
+        elif re.search('[\w\s]+\.\s[\w\s]*', row["authorGroup"]) is not None: # look for hierarchical names, like "England and Wales. Parliament. House of Commons" 
+            possible_non_persons += [row]
+            print("Is this a non-person?")
+            print(row["authorGroup"])
+        else: #to do: write a function for filtering specific stopwords, such as "company"
             likely_persons += [row]
-            #print("Is this a person?")
-            #print(row["authorGroup"])
-        else:
-            possible_non_persons += [row] #to do: write a function for filtering specific stopwords, such as "company"
-            #print("Is this a non-person?")
-            #print(row["authorGroup"])
+            print("Is this a person?")
+            print(row["authorGroup"])
 
 #testing: results need work
 print("These might not be people")
