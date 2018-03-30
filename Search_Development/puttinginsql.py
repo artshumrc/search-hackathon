1abbd1fd380b2b586f182b440d8c94aeddb98358
niharikasingh <niharikasingh@users.noreply.github.com>
2018-03-29 23:44:15 -0400

    Update puttinginsql.py

--- Search_Development/puttinginsql.py
+++ Search_Development/puttinginsql.py
@@ -6,18 +6,23 @@ from collections import Counter
 import string
 import pymongo
 
+# create word processors, i.e., stemmer, punctuation remover
 ps = PorterStemmer()
 stops = set(stopwords.words("english")) | set(stopwords.words("french")) | set(stopwords.words("german"))
 punc = str.maketrans({key: None for key in string.punctuation})
+# create mongo database
+client = pymongo.MongoClient("mongodb+srv://searchhackathon:searchhackathon@search-hackathon-gr4pq.mongodb.net/test")
+db = client.test
+coll = db.collection
 
 FILENAME = '000001'
 
-contents = Path('testfiles/000001.txt').read_text() # get all data from this file
+contents = Path('testfiles/' + FILENAME + '.txt').read_text() # get all data from this file
 wordlist = [] # where we store all the words from this file
 
 sent_text = sent_tokenize(contents) # split text file into sentences
 for sentence in sent_text:
-    sentence = sentence.translate(punc)
+    sentence = sentence.translate(punc) # remove punctuation
     words = word_tokenize(sentence) # split each sentence into words
     words = [ps.stem(word) for word in words] # stem words
     words = [word for word in words if word not in stops] # remove stopwords
@@ -27,11 +32,6 @@ for sentence in sent_text:
 # now we have all the words in this file
 wordcount = Counter(wordlist) # count words
 print(wordcount)
-
-# create mongo database
-client = pymongo.MongoClient("mongodb+srv://searchhackathon:searchhackathon@search-hackathon-gr4pq.mongodb.net/test")
-db = client.test
-coll = db.collection
 
 for word in wordcount:
     if (coll.find_one({'word': word}) == None):
