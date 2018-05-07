83863ef64ea68d66f05fd7b9ba28410fd56c1fac
niharikasingh <niharikasingh@users.noreply.github.com>
2018-05-07 16:02:47 -0400

    Updating splitter.py to clean text too

--- Search_Development/splitter.py
+++ Search_Development/splitter.py
@@ -1,20 +1,36 @@
 import os
 import sys
+from collections import Counter
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
+import string
 
+ps = PorterStemmer()
+stops = set(stopwords.words("english")) | set(stopwords.words("french")) | set(stopwords.words("german"))
+punc = str.maketrans({key: None for key in string.punctuation})
 
 def splitter(n, s):
     pieces = s.split()
     return (" ".join(pieces[i:i+n]) for i in range(0, len(pieces), n))
 
+def cleaner(s):
+    s = s.translate(punc) # remove punctuation
+    s = s.lower() # lowercase words
+    words = word_tokenize(s) # split each sentence into words
+    words = [word for word in words if word not in stops] # remove stopwords
+    words = [ps.stem(word) for word in words] # stem words
+    return words
+
 
 # Get input and output directories
-user_input_dir = input("Please specify path to input directory:")
+user_input_dir = "ecco.data.alltext/clean2" # input("Please specify path to input directory:")
 if os.path.isdir(user_input_dir):
     input_path = user_input_dir
 else:
     print("Invalid path.")
     sys.exit()
-file_list = [x for x in os.listdir(input_path) if x.endswith(".txt")]
+file_list = [x for x in os.listdir(input_path) if x.endswith(".txt")][49835:]
 print("{}{}".format(len(file_list)," files"))
 
 if not os.path.exists(user_input_dir + "/output"):
@@ -42,4 +58,9 @@ for filename in file_list:
         file = open(input_path + "/output" + "/" + subdir + "/" + str(chunk).zfill(6) + ".txt", "w")
         file.write(piece)
         file.close()
+        countfile = open(input_path + "/output" + "/" + subdir + "/" + str(chunk).zfill(6) + ".vol.tsv", "w")
+        wordcounts = Counter(cleaner(piece))
+        for word in wordcounts:
+            countfile.write(word + "\t" + str(wordcounts[word]) + "\n")
+        countfile.close()
     print("{}{}".format(chunk," chunks processed"))
