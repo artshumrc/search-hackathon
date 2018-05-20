8accde35d89aba0ae6671f2aad4c965211539973
niharikasingh <niharikasingh@users.noreply.github.com>
2018-05-20 00:04:33 -0400

    Scripts to upload to Elasticsearch

---
+++ Search_Development/metadataprocessing.py
@@ -0,0 +1,63 @@
+import csv
+import datetime
+import json
+import re
+
+outputdict = {}
+
+for i in range(1, 5):
+    with open('metadata_' + str(i) + '.csv') as metadatafile:
+        filereader = csv.reader(metadatafile)
+        for line in filereader:
+            if (i == 1):
+                line = line[0:7] + line[8:]
+            documentID = line[0]
+            if (documentID == '') or (documentID in outputdict) or (not documentID.isdigit()):
+                print("Error in documentID, line is: ", line)
+            else:
+                documentID = int(documentID)
+                estcid = line[1]
+                pubDate = line[2]
+                if ((not pubDate.isdigit()) or (len(pubDate) != 8)):
+                    print("Error in pubDate,", pubDate.isdigit(), len(pubDate), pubDate, " line is: ", line)
+                    continue
+                year = int(pubDate[0:4])
+                month = int(pubDate[4:6])
+                day = int(pubDate[6:8])
+                pubDate = datetime.date(year, month, day)
+                finalRecon = line[6]
+                finalRecon = re.sub(r'\[\]', '', finalRecon)
+                gender = line[11]
+                if (line[13] != ''):
+                    birthFinal = int(line[13])
+                else:
+                    birthFinal = -1
+                if (line[15] != ''):
+                    deathFinal = int(line[15])
+                else:
+                    deathFinal = -1
+                titleGroup = line[17].strip()
+                imprint = line[18].strip()
+                publicationPlace = line[19]
+                if (line[20].isdigit()):
+                    totalPages = int(line[20])
+                else:
+                    totalPages = -1
+                url = line[8]
+
+                outputdict[documentID] = {
+                    "documentID": documentID,
+                    "estcid": estcid,
+                    "pubDate": pubDate,
+                    "finalRecon": finalRecon,
+                    "gender": gender,
+                    "birthFinal": birthFinal,
+                    "deathFinal": deathFinal,
+                    "titleGroup": titleGroup,
+                    "imprint": imprint,
+                    "publicationPlace": publicationPlace,
+                    "totalPages": totalPages,
+                    "url": url
+                }
+
+print(json.dumps(outputdict, indent=2))
---
+++ Search_Development/transfertoelastic.py
@@ -0,0 +1,92 @@
+from elasticsearch import Elasticsearch, helpers
+import os, json, copy
+
+es = Elasticsearch()
+
+stemSettings = {
+  "settings": {
+    "analysis": {
+      "analyzer": {
+        "english_exact": {
+          "tokenizer": "standard",
+          "filter": [
+            "lowercase"
+          ]
+        }
+      }
+    }
+  },
+  "mappings": {
+    "_doc": {
+      "properties": {
+        "body": {
+          "type": "text",
+          "analyzer": "english",
+          "term_vector": "with_positions_offsets_payloads",
+          "store" : True,
+          "fields": {
+            "exact": {
+              "type": "text",
+              "analyzer": "english_exact"
+            }
+          }
+        },
+        "documentID": { "type": "integer" },
+        "estcid": { "type": "text" },
+        "pubDate": { "type": "date" },
+        "finalRecon": { "type": "text" },
+        "gender": { "type": "text" },
+        "birthFinal": { "type": "integer" },
+        "deathFinal": { "type": "integer" },
+        "titleGroup": { "type": "text" },
+        "imprint": { "type": "text" },
+        "publicationPlace": { "type": "text" },
+        "totalPages": { "type": "integer" },
+        "url": { "type": "text" },
+        "totalvolumes": { "type": "integer" },
+        "volumenumber": { "type": "integer" },
+      }
+    }
+  } # from https://www.elastic.co/guide/en/elasticsearch/reference/master/mixing-exact-search-with-stemming.html
+}
+
+with open('metadata.json') as f:
+    metadata = json.load(f)
+
+es.indices.create(index='smol-ecco', ignore=400, body=stemSettings) # normal index
+loop_index = 0
+bulk_data = []
+
+for subdir, dirs, files in os.walk(os.getcwd() + '/data'):
+    for filename in files:
+        if (filename[-4:] == '.txt'):
+            with open(os.path.join(subdir, filename)) as textfile:
+
+                loop_index += 1
+
+                text = textfile.read()
+                docid = subdir.split('/')[-1]
+                sourcedict = copy.deepcopy(metadata[docid])
+                sourcedict["body"] = text
+
+                fileid = docid + "_" + filename[:-4]
+
+                data_dict = {
+                    '_op_type': 'index',
+                    '_index': 'smol-ecco',
+                    '_type': '_doc',
+                    '_id': fileid,
+                    '_source': sourcedict
+                }
+                bulk_data.append(data_dict)
+
+                # es.create(index='smol-ecco', body=text, doc_type='_doc', id=fileid)
+                print(fileid)
+
+                if (loop_index % 25 == 0):
+                    helpers.bulk(client=es, actions=bulk_data)
+                    es.indices.refresh()
+                    bulk_data = []
+
+helpers.bulk(client=es, actions=bulk_data)
+es.indices.refresh()
