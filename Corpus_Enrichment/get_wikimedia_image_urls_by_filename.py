3ee93e7d5c90a0cc318a54656f5f737480a25743
eslao <eslao@fas.harvard.edu>
2018-05-20 17:58:26 -0400

    Add notes

--- Corpus_Enrichment/get_wikimedia_image_urls_by_filename.py
+++ Corpus_Enrichment/get_wikimedia_image_urls_by_filename.py
@@ -1,3 +1,6 @@
+# this assumes you've already fetched image filenames via OpenRefine, SPARQL, or whatever
+# useful background: https://stackoverflow.com/questions/34393884/how-to-get-image-url-property-from-wikidata-item-by-api?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+
 import hashlib
 filename = 'Ned_Ward_by_Michael_Vandergucht.jpg'.encode('utf-8') # replace string with any wikimedia filename
 directory = hashlib.md5(filename).hexdigest()[0]  + "/" + hashlib.md5(filename).hexdigest()[0] + hashlib.md5(filename).hexdigest()[1] + "/"
