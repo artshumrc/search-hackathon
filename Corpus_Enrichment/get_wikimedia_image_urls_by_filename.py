c427f483b45c056b4ead6e446da705740247a166
eslao <eslao@fas.harvard.edu>
2018-05-21 06:42:54 -0400

    Add notes

--- Corpus_Enrichment/get_wikimedia_image_urls_by_filename.py
+++ Corpus_Enrichment/get_wikimedia_image_urls_by_filename.py
@@ -4,12 +4,15 @@
 import hashlib
 import pandas as pd
 
+infile = 'example.csv' # replace with original csv; must have a column called 'image filename'
+outfile = 'output.csv' #replace with desired output filename
+
 def wikidata_image_url(filename):
     filename = filename.encode('utf-8')
     directory = hashlib.md5(filename).hexdigest()[0]  + "/" + hashlib.md5(filename).hexdigest()[0] + hashlib.md5(filename).hexdigest()[1] + "/"
     fileurl = str('https://upload.wikimedia.org/wikipedia/commons/') + directory + filename.decode("utf-8")
     return fileurl
 
-csv_input = pd.read_csv('example.csv')
-csv_input['image url'] = csv_input['image filename'].apply(wikidata_image_url)
-csv_input.to_csv('output.csv', index=False)
+csv_input = pd.read_csv(infile)
+csv_input['image url'] = csv_input['image filename'].apply(wikidata_image_url) # adds new column called 'image url' to end of each row
+csv_input.to_csv(outfile, index=False)
