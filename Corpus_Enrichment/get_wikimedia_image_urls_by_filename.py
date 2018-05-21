1abac2b33c7e4becb40b6f29e53351dde114fc11
eslao <eslao@fas.harvard.edu>
2018-05-21 06:40:47 -0400

    add csv manipulation

--- Corpus_Enrichment/get_wikimedia_image_urls_by_filename.py
+++ Corpus_Enrichment/get_wikimedia_image_urls_by_filename.py
@@ -2,6 +2,14 @@
 # useful background: https://stackoverflow.com/questions/34393884/how-to-get-image-url-property-from-wikidata-item-by-api?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
 
 import hashlib
-filename = 'Ned_Ward_by_Michael_Vandergucht.jpg'.encode('utf-8') # replace string with any wikimedia filename
-directory = hashlib.md5(filename).hexdigest()[0]  + "/" + hashlib.md5(filename).hexdigest()[0] + hashlib.md5(filename).hexdigest()[1] + "/"
-fileurl = str('https://upload.wikimedia.org/wikipedia/commons/') + directory + filename.decode("utf-8")
+import pandas as pd
+
+def wikidata_image_url(filename):
+    filename = filename.encode('utf-8')
+    directory = hashlib.md5(filename).hexdigest()[0]  + "/" + hashlib.md5(filename).hexdigest()[0] + hashlib.md5(filename).hexdigest()[1] + "/"
+    fileurl = str('https://upload.wikimedia.org/wikipedia/commons/') + directory + filename.decode("utf-8")
+    return fileurl
+
+csv_input = pd.read_csv('example.csv')
+csv_input['image url'] = csv_input['image filename'].apply(wikidata_image_url)
+csv_input.to_csv('output.csv', index=False)
