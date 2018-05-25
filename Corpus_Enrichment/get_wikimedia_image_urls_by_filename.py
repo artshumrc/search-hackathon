ff7f291094d7ec35feafbd08addfbedfdf7a3004
Cole Crawford <cole_crawford@harvard.edu>
2018-05-25 13:06:24 -0400

    Added functions for processing vols with regex

--- Corpus_Enrichment/get_wikimedia_image_urls_by_filename.py
+++ Corpus_Enrichment/get_wikimedia_image_urls_by_filename.py
@@ -3,8 +3,9 @@
 
 import hashlib
 import pandas as pd
+import re
 
-infile = 'example.csv' # replace with original csv; must have a column called 'image'
+infile = 'ecco_metadata_recon3_100kto150k.csv' # replace with original csv; must have a column called 'image'
 outfile = 'output.csv' #replace with desired output filename
 
 def wikidata_image_url(filename):
@@ -15,8 +16,32 @@ def wikidata_image_url(filename):
         directory = hashlib.md5(filename).hexdigest()[0]  + "/" + hashlib.md5(filename).hexdigest()[0] + hashlib.md5(filename).hexdigest()[1] + "/"
         fileurl = str('https://upload.wikimedia.org/wikipedia/commons/') + directory + filename.decode("utf-8")
         return fileurl
+    
+def get_volNum(volumeGroup):
+    volNum = re.search(r'Volume\s\d+', volumeGroup)
+    if volNum is not None:
+        volNum = volNum.group(0).split()[1]
+        return volNum
+    else:
+        return ''
+
+def get_numVols(volumeGroup):
+    #numVols = re.search(r'Volume\s\d+\s+\\t\\t\\t\s\d+', volumeGroup)
+    numVols = re.search(r'Volume\s\d+\s+\d+', volumeGroup)
+    #print(volumeGroup)
+    if numVols is not None:
+        numVols = numVols.group(0).split()[2]
+        #print(numVols)
+        return numVols
+    else:
+        return ''
 
 df = pd.read_csv(infile)
+cols = ['documentID', 'pubDate', 'pubYearNum','birthFinal', 'deathFinal', 'totalPages']
+for col in cols:
+    df[col] = df[col].apply(lambda x: int(x) if x == x else "")
 df.fillna('',inplace=True)
-df['image_url'] = df['image'].apply(wikidata_image_url) # adds new column called 'image_url' to end of each row
+df['image_url'] = df['image filename'].apply(wikidata_image_url) # adds new column called 'image_url' to end of each row
+df['volNum'] = df['volumeGroup'].apply(get_volNum) #Adds volNum
+df['numVols'] = df['volumeGroup'].apply(get_numVols) #Adds numVols
 df.to_csv(outfile, index=False)
